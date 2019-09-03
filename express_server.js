5d54436594f195bcd55e84eaedf19fc190c23d8e
Zongxi Li <zongxiliwork@gmail.com>
2019-09-03 01:17:46 +0000

    Long URL codes added

--- express_server.js
+++ express_server.js
@@ -35,12 +35,18 @@ app.get("/set", (req, res) => {
   res.send(`a = ${a}`);
  });
 
-app.listen(PORT, () => {
-  // Notify the user when the Server listening the Client
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  // console.log("HERERERE");
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+app.listen(PORT, () => {
+  // Notify the user when the Server listening the Client
+  console.log(`Example app listening on port ${PORT}!`);
 });
