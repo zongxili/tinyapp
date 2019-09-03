0afe5b1de9482e95d0c92659d276dad9ad4a4416
Zongxi Li <zongxiliwork@gmail.com>
2019-09-03 16:53:04 +0000

    Long Url generator added

--- express_server.js
+++ express_server.js
@@ -9,8 +9,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
+
+function generateRandomString() {
+  var returnOutput = "";
+  var charList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for ( var i = 0; i < 6; i++ ) {
+    returnOutput += charList.charAt(Math.floor(Math.random() * charList.length));
+  }
+  return returnOutput;
+}
 
 app.get("/", (req, res) => {
   // Message disappear in the loaded page
@@ -46,18 +55,25 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  console.log("Here is the NEW page.");
   res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  console.log("Here is the SHORT URLS page.");
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
+  // codes
+  console.log(typeof req.body);
+  let shortURL = generateRandomString();
+  newLongURL = req.body["longURL"];
+  urlDatabase[newLongURL] = shortURL;
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
