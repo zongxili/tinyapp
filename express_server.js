271012eff854204dda987235792fdebaf97e7bf8
Zongxi Li <zongxiliwork@gmail.com>
2019-09-03 02:08:58 +0000

    POST method added

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -31,19 +34,31 @@ app.get("/set", (req, res) => {
   res.send(`a = ${a}`);
  });
  
- app.get("/fetch", (req, res) => {
+app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
- });
+});
+
 
 app.get("/urls", (req, res) => {
+  // console.log("Here is the URLS HOME page.");
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  console.log("Here is the NEW page.");
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-  // console.log("HERERERE");
+  console.log("Here is the SHORT URLS page.");
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
