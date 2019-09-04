b636814099c3714e508ce670c147e5d65fc9d7a1
Zongxi Li <zongxiliwork@gmail.com>
2019-09-04 23:46:14 +0000

    User Registration

--- express_server.js
+++ express_server.js
@@ -3,13 +3,32 @@ var cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080;
 
-// Set up field
+// Set up field: initialize the Server Environment
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
-var morgan = require('morgan')
+var morgan = require('morgan');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
 app.use(cookieParser());
+
+const users = { 
+    "testingID123": {
+    id: "testingID123", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  }
+}
+
+const emailLookHeler = function(userEmail) {
+  let arrValue = Object.values(users);
+  for (let i = 0; i < arrValue.length; i++){
+    console.log("TEST________", arrValue[i]["email"], userEmail)
+  	if (arrValue[i]["email"] === userEmail ) {
+        return arrValue[i]["id"];
+    }
+  }
+  return false;
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -52,19 +71,46 @@ app.get("/fetch", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  console.log("Here is the URLS HOME page. " + res.cookies);
-  let templateVars = { urls: urlDatabase, username: req.cookies['username'] };
+  console.log("Here is the URLS GET page.");
+  const userID = req.cookies["user_id"];
+  
+  const user = users[userID];
+  // const passinUserEmail = user["email"];
+  let templateVars = { urls: urlDatabase, passinUser: user };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  let templateVars = { urls: urlDatabase, username: req.cookies['username'] };
+  console.log("In the URLS NEW GET page");
+
+  const userID = req.cookies["user_id"];
+  const user = users[userID];
+  let templateVars = { urls: urlDatabase, passinUser: user };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies['username']};
+
+  const userID = req.cookies["user_id"];
+  const user = users[userID];
+
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], passinUser: user};
   res.render("urls_show", templateVars);
+});
+
+app.get("/register", (req, res) => {
+  const userID = req.cookies["user_id"];
+  const user = users[userID];
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], passinUser: user};
+  res.render("urls_newTem", templateVars);
+});
+
+app.get("/login", (req, res) => {
+  console.log("IN LOGIN GET");
+  const longURL = urlDatabase[req.params.shortURL];
+
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], passinUser: undefined};
+  res.render("urls_login", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -75,9 +121,39 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 // POST methods
+app.post("/register", (req, res) => {
+  console.log();
+  const randomNewUserID = generateRandomString();
+  console.log("randomNewUserID is :" + randomNewUserID);
+  const userEmail = req.body.email;
+  const userPassword = req.body.password;
+  console.log("userEmail: " + userEmail);
+  console.log("userPassword: " + userPassword);
+
+  // Handle Registration Errors
+  if (userEmail === undefined || userPassword === undefined){
+    res.status(403).send("Oh uh, one of or both of Email and password fields are UNDEFINED.");
+  }
+  else if (userEmail === "" || userPassword === ""){
+    res.status(403).send("Oh uh, one of or both of Email and password fields are empty.");
+  }
+  if (emailLookHeler(userEmail)) {
+    res.status(403).send("Oh uh, it seems like this email is already registered.");
+  }
+  users[randomNewUserID] = {
+    id: randomNewUserID,
+    email: userEmail,
+    password: userPassword
+  };
+  // this needs to set the new cookie
+  // but how come we need set a new cookie but not just use the res.cookie?
+  res.cookie("user_id", randomNewUserID);
+  res.redirect("/urls");
+});
+
 app.post("/urls", (req, res) => {
   // codes
-  console.log("IN THE URLS PAGE:" + typeof req.body);
+  console.log("IN THE URLS POST PAGE:" + typeof req.body);
   let shortURL = generateRandomString();
   newLongURL = req.body["longURL"];
   urlDatabase[newLongURL] = shortURL;
@@ -92,20 +168,38 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  const inputName = req.body.username;
-  console.log(req.cookies);
-  res.cookie("username", inputName);
-  console.log(req.cookies);
-  res.redirect("/urls");
+  // const inputName = req.body.user_id;
+  // // console.log(req.cookies);
+  // const userID = req.cookies["user_id"];
+  // const user = users[userID];
+  // let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], passinUser: user};
+  // res.render("urls_newTem", templateVars);
+  
+  // res.cookie("user_id", inputName);
+  const email = req.body.email;
+
+  const password = req.body.password;
+  if (!emailLookHeler(email) || users[emailLookHeler(email)]["password"] !== password ) {
+    res.status(403).send("Oh uh, account doesn't exist or the password doesn't match.");
+  }
+  else{
+    // this needs to set the new cookie
+    // but how come we need set a new cookie but not just use the res.cookie?
+
+    res.cookie("user_id", emailLookHeler(email));
+    // console.log(req.cookies);
+    res.redirect("/urls");
+  }
+
 });
 
 app.post("/logout/", (req, res) => {
   console.log("-------------------------------------------------------------------");
   console.log("In the LOGOUT page");
-  const inputName = req.cookies.username;
+  const inputName = req.cookies.user_id;
   console.log(typeof inputName);
-  // console.log(req.cookies.username);
-  res.clearCookie("username");
+  // console.log(req.cookies.user_id);
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
 
